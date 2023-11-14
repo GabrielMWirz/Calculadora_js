b5247fbb78444825a2b82815ddf1a308a608211f
GabrielMWirz <143511966+GabrielMWirz@users.noreply.github.com>
2023-11-14 11:55:27 -0300

    Create divi.js

---
+++ divi.js
@@ -0,0 +1,7 @@
+function divi()
+{
+  let num1 = parseFloat(document.getElementById("num1").value);
+  let num2 = parseFloat(document.getElementById("num2").value);
+  let resul = num1 / num2;
+  document.getElementById("resul").innerText = "Resultado: " + resul;
+}
